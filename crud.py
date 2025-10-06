9705f0d9e19c35952ed64bde8c1c559287f01980
Gabriel Santos Silva <gabriel.s.silva@senai.aluno.br>
2025-10-06 14:27:20 -0300

    função de atualizar feita ✔

--- crud.py
+++ crud.py
@@ -27,6 +27,17 @@ def listar_aluos():
             cursor.close()
             conexao.close()
 lista = listar_aluos()
-print(lista)
 for aluno in lista:
     print(aluno[0], aluno[1])
+def atualizar_idade(id_aluno, nova_idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("UPDATE alunos SET idade = %s WHERE id = %s"(nova_idade, id_aluno))
+            conexao.commit()
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao tentar atualizar o aluno:{erro}")
+        finally:
+            cursor.close()
+            conexao.close()

bb5d827272d1174abdc25e839d8c66ec5bb3e4cb
Gabriel Santos Silva <gabriel.s.silva@senai.aluno.br>
2025-10-06 15:47:45 -0300

    add menu com função inserir✔

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from crud import criar_aluno, listar_aluos, atualizar_idade, deletar_aluno
+
+st.set_page_config(page_title="Gerenciamento de alunos", page_icon="👨‍🎓")
+
+st.title("Sistema de alunos com PostgreSQL")
+
+menu = st.sidebar.radio("Menu", ["Inserir", "Listar", "Atualizar", "Deletar"])
+
+if menu == "Inserir":
+    st.subheader("➕ Inserir alunos")
+    nome = st.text_input("Nome", placeholder="Seu nome")
+    idade = st.number_input("Idade", min_value=16, step=1)
+    if st.button("Cadastrar"):
+        if nome.strip() != "":
+            criar_aluno(nome, idade)
+            st.success(f"Aluno {nome} inserido com sucesso!")
+        else:
+            st.warning("O campo nome não pode ser vazio.")
